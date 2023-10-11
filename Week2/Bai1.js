015d5471037c04cf675cb2064b611c0f2baba778
trungdung70199 <trungdung70199@gmail.com>
2023-10-11 22:45:25 +0900

    up bai tap

---
+++ Week2/Bai1.js
@@ -0,0 +1,21 @@
+// Khai bao bien
+
+let i = 10;
+console.log(i);
+console.log("Type of i:")
+console.log(typeof(i));
+
+let f = 20.5;
+console.log(f);
+console.log("Type of f:")
+console.log(typeof(f));
+
+let b = true;
+console.log(b);
+console.log("Type of b:")
+console.log(typeof(b));
+
+let s = "Hanoi";
+console.log(s)
+console.log("Type of s:")
+console.log(typeof(s));
---
+++ Week2/Bai2.js
@@ -0,0 +1,7 @@
+// Dien tich hinh chu nhat
+
+let width = 20;
+let height = 10;
+let Square = height * width;
+
+console.log("Dien tich hcn la:", Square);
