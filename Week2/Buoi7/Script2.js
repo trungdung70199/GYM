340d3bfe5a14858e9c3d5879aadf2d0363f0144f
trungdung70199 <trungdung70199@gmail.com>
2023-10-13 21:37:04 +0900

    Up button file

---
+++ Week2/Buoi7/Script2.js
@@ -0,0 +1,15 @@
+// Tinh chu vi dien tich hcn
+
+let Width = parseInt(prompt("Input width:"));
+
+let Height = parseInt(prompt("Input height"));
+
+let C_vi = (Width + Height) * 2;
+
+let D_tich = Width * Height;
+// Output dien tich
+alert("Dien tich hcn la:")
+alert(D_tich);
+// Output C_vi
+alert("C_vi hcn la:");
+alert(C_vi);
---
+++ Week2/Buoi7/Variable.js
@@ -0,0 +1,13 @@
+// Input number n
+
+// Type casting
+let n = parseInt(prompt("Input a number"));
+// Out put type n 
+alert(typeof(n));
+
+let result = n ** 2;
+
+alert("Binh phuong cua so n la:")
+
+alert(result);
+
