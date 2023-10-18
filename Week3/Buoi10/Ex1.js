7ffc0a24a3f5f270491529c79aa158e8de8b3796
trungdung70199 <trungdung70199@gmail.com>
2023-10-19 00:03:31 +0900

    up file

---
+++ Week3/Buoi10/Ex1.js
@@ -0,0 +1,19 @@
+/// Change name on h1 tag
+// Cach 1
+let titleElement = document.getElementById("head1");
+let lesson1Ele = document.getElementById("lesson - 1");
+let lesson2Ele = document.getElementById("lesson - 2");
+let lesson3Ele = document.getElementById("lesson - 3");
+// Out put
+
+titleElement.innerHTML = "JavaScript Course Syllabus";
+lesson1Ele.innerText = "Welcome to JavaScript";
+lesson2Ele.innerText = "JavaScrip Fundamental";
+lesson3Ele.innerText = "JavaScript Intermediate";
+
+
+// Cach 2: Su dung function
+
+// function myFunction() {
+//     document.getElementById("head1").innerHTML = "JavaScript Course Syllabus";
+// }
---
+++ Week3/Buoi10/Ex2.js
@@ -0,0 +1,18 @@
+let btnUpdate = document.getElementById("btn-update");
+// Them su kien vao 
+btnUpdate.addEventListener('click', () => {
+    alert("Button update is clicked");
+    console.log("Button is clicked");
+    // Change Color
+    let HeadColor = document.getElementById("head1");
+    HeadColor.style.color = "red";
+    // Move tropical
+    let trop = document.getElementById("Trop");
+    trop.remove();
+    //add Fruit Yogurt
+    let newYogurt = document.createElement("li");
+    newYogurt.innerText = "Fruit Yogurt";
+    let Yogurt = document.getElementById("Drink-list");
+    Yogurt.appendChild(newYogurt);
+});
+
