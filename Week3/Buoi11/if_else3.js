63d1ddd407ce53f30d36f07e4841dba80a4f72c9
trungdung70199 <trungdung70199@gmail.com>
2023-10-22 22:22:46 +0900

    update if_else3.js

--- Week3/Buoi11/if_else3.js
+++ Week3/Buoi11/if_else3.js
@@ -3,14 +3,14 @@ btnAction.addEventListener("click", () => {
     let billValue = parseFloat(document.getElementById("bill-value").value);
     // console.log(typeof(billValue));
     // billValue > 4000000
-    if (billValue > 4000000) {
+    if (billValue >= 4000000) {
         alert(`Ban da dc giam gia ${billValue * 0.2}VND`);
         // <Cap nhat lai gia tri don hang
         billValue *= 0.8;
-    } else if (2500000 < billValue && billValue<=4000000){
-        alert(`Ban da dc giam gia ${billValue * 0.15}VND`);
+    } else if (billValue >= 2500000){
+        alert(`Ban da dc giam gia ${billValue * 0.15}VND`); 
         billValue *= 0.85;
-    } else if (150000 < billValue && billValue<= 2500000){
+    } else if (billValue >= 1500000){
         alert(`Ban da dc giam gia ${200000}VND`);
         billValue -= 200000
         //billValue
