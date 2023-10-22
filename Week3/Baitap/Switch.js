035fdd97d767e1946d1061ef6800adb433269293
trungdung70199 <trungdung70199@gmail.com>
2023-10-23 00:50:59 +0900

    up Switch.js

---
+++ Week3/Baitap/Switch.js
@@ -0,0 +1,15 @@
+// Switch - case
+
+switch (browser) {
+    case 'Edge':
+        alert("You've got the Edge");
+        break;
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+        alert("Okay we support these browser too");
+        break;
+    default:
+        alert("We hope that this page looks ok!");
+}
