78da04f5efb3f58ef0637435c71558e8f5b61d1b
trungdung70199 <trungdung70199@gmail.com>
2023-11-13 23:46:50 +0900

    up

---
+++ Week7/Buoi19/demo_function2.js
@@ -0,0 +1,10 @@
+// Viet ham gioi thieu ban than
+// Nhap vao ten
+// Khoa hoc
+let name = 'Dung';
+let course = 'JS';
+function greeting(name, course) {
+	document.write(`My name is ${name}. My course is ${course}`);
+}
+
+greeting('Dung', 'JS');
---
+++ Week7/Buoi19/demo_function3.js
@@ -0,0 +1,10 @@
+// nhap vao 2 so a, b in ra sum
+
+// function calcSum(a, b) {
+// 	console.log(a + b);
+// }
+
+// Arrow function
+
+let calcSum = (a, b) => console.log(a + b);
+calcSum(10, 20);
