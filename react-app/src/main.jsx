283c94e3a0c16cedf65a5544b7e2d5a8f74f2240
trungdung70199 <trungdung70199@gmail.com>
2023-11-23 00:23:09 +0900

    add root err page update json

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import Root from './routes/root';
+import ErrorPage from './error-page';
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"; 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
