4d8da8323ded9325e817d40ff42cf77c0ec471f6
trungdung70199 <trungdung70199@gmail.com>
2023-11-24 22:15:11 +0900

    up contact update root

--- react-app/src/routes/root.jsx
+++ react-app/src/routes/root.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Root() {
     return (
@@ -31,15 +32,19 @@ export default function Root() {
                 <nav>
                     <ul>
                         <li>
-                            <a href={`contacts/1`}>Your Name</a>
+                            <Link to={`contacts/1`}>Your Name</Link>
+                            {/* <a href={`contacts/1`}>Your Name</a> */}
                         </li>
                         <li>
-                            <a href={`contacts/2`}>Your Friend</a>
+                            <Link to={`contacts/2`}>Your Friend</Link>
+                            {/* <a href={`contacts/2`}>Your Friend</a> */}
                         </li>
                     </ul>
                 </nav>
             </div>
-            <div id="detail"></div>
+            <div id="detail">
+                <Outlet />
+            </div>
         </>
-    )
+    );
 }
